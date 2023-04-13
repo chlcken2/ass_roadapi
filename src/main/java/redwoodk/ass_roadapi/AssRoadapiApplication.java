bb5fcb726dd043db8a5343db370b5accd85b53d6
ckdgu <ckdgus988@naver.com>
2023-04-13 14:00:06 +0900

    Initial commit

---
+++ src/main/java/redwoodk/ass_roadapi/AssRoadapiApplication.java
@@ -0,0 +1,13 @@
+package redwoodk.ass_roadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssRoadapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssRoadapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/redwoodk/ass_roadapi/AssRoadapiApplicationTests.java
@@ -0,0 +1,13 @@
+package redwoodk.ass_roadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssRoadapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
