60d2360c3230689ef625a4fc81f062d54f6e819f
ckdgu <ckdgus988@naver.com>
2023-04-13 15:21:54 +0900

    FEATURE: 도로명 주소 API 등록

---
+++ src/main/java/redwoodk/ass_roadapi/controller/TestController.java
@@ -0,0 +1,14 @@
+package redwoodk.ass_roadapi.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
